43488237206764efce2243feeeb88cc759321063
Abhijith P <abhijithp@Abhijiths-MacBook-Air.local>
2025-12-08 10:57:01 +0530

    Added profile feature and profile-picture navigation

--- tweets/views.py
+++ tweets/views.py
@@ -16,37 +16,44 @@ def tweet_create(request):
         photo = request.FILES.get("photo")
         Tweet.objects.create(user=request.user, text=text, photo=photo)
         return redirect("tweet_list")
+
     return render(request, "tweets/tweet_form.html")
 
 
 @login_required
 def tweet_edit(request, tweet_id):
     tweet = get_object_or_404(Tweet, id=tweet_id, user=request.user)
+
     if request.method == "POST":
         tweet.text = request.POST.get("text")
         if request.FILES.get("photo"):
             tweet.photo = request.FILES.get("photo")
         tweet.save()
         return redirect("tweet_list")
+
     return render(request, "tweets/tweet_form.html", {"tweet": tweet})
 
 
 @login_required
 def tweet_confirm_delete(request, tweet_id):
     tweet = get_object_or_404(Tweet, id=tweet_id, user=request.user)
+
     if request.method == "POST":
         tweet.delete()
         return redirect("tweet_list")
+
     return render(request, "tweets/tweet_confirm_delete.html", {"tweet": tweet})
 
 
 @login_required
 def tweet_like_toggle(request, tweet_id):
     tweet = get_object_or_404(Tweet, id=tweet_id)
+
     if request.user in tweet.likes.all():
         tweet.likes.remove(request.user)
     else:
         tweet.likes.add(request.user)
+
     return redirect("tweet_list")
 
 
@@ -54,20 +61,27 @@ def tweet_like_toggle(request, tweet_id):
 def comments_view(request, tweet_id):
     tweet = get_object_or_404(Tweet, id=tweet_id)
     comments = Comment.objects.filter(tweet=tweet, parent=None).order_by("-created_at")
-    return render(request, "tweets/comments.html", {"tweet": tweet, "comments": comments})
+
+    return render(request, "tweets/comments.html", {
+        "tweet": tweet,
+        "comments": comments
+    })
 
 
 @login_required
 def comment_add(request, tweet_id):
     tweet = get_object_or_404(Tweet, id=tweet_id)
+
     if request.method == "POST":
         text = request.POST.get("text")
         parent_id = request.GET.get("parent")
+
         if parent_id:
-            parent = get_object_or_404(Comment, id=parent_id)
+            parent = Comment.objects.get(id=parent_id)
             Comment.objects.create(user=request.user, tweet=tweet, text=text, parent=parent)
         else:
             Comment.objects.create(user=request.user, tweet=tweet, text=text)
+
     return redirect("comments_view", tweet_id=tweet_id)
 
 
@@ -86,6 +100,7 @@ def register(request):
         user = User.objects.create_user(username=username, password=password)
         login(request, user)
         return redirect("tweet_list")
+
     return render(request, "registration/register.html")
 
 
@@ -93,16 +108,23 @@ def register(request):
 def user_profile(request, username):
     user = get_object_or_404(User, username=username)
     tweets = Tweet.objects.filter(user=user).order_by("-created_at")
-    return render(request, "profile/profile.html", {"profile_user": user, "tweets": tweets})
+
+    return render(request, "profile/profile.html", {
+        "profile_user": user,
+        "tweets": tweets
+    })
 
 
 @login_required
 def edit_profile(request):
     profile = request.user.profile
+
     if request.method == "POST":
         profile.bio = request.POST.get("bio")
         if request.FILES.get("image"):
             profile.image = request.FILES.get("image")
+
         profile.save()
         return redirect("user_profile", username=request.user.username)
+
     return render(request, "profile/edit_profile.html", {"profile": profile})
