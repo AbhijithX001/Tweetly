bc46799bd2adfd2f7a7afdc070dd3409196a3599
Abhijith P <abhijithp@Abhijiths-MacBook-Air.local>
2025-12-27 18:23:10 +0530

    fix render allowed hosts and secret key

--- tweetly_project/settings.py
+++ tweetly_project/settings.py
@@ -7,17 +7,18 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("SECRET_KEY", "dummy-secret-key")
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
-    "tweetly-production.up.railway.app",
-    "127.0.0.1",
     "localhost",
+    "127.0.0.1",
+    ".onrender.com",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://tweetly-production.up.railway.app"
+    "https://*.onrender.com",
 ]
 
 INSTALLED_APPS = [
@@ -69,7 +70,7 @@ if DATABASE_URL:
         "default": dj_database_url.parse(
             DATABASE_URL,
             conn_max_age=600,
-            ssl_require=True
+            ssl_require=True,
         )
     }
 else:
@@ -88,8 +89,11 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LANGUAGE_CODE = "en-us"
+
 TIME_ZONE = "UTC"
+
 USE_I18N = True
+
 USE_TZ = True
 
 STATIC_URL = "/static/"
