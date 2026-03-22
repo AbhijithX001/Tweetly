9bad676b9c5b7bc6b68eecec8cbbc54a680c7c2c
AbhijithX001 <abhijithprakash22334a@gmail.com>
2026-03-22 09:45:26 +0530

    fix media serving in production and add default avatar

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn tweetly_project.wsgi
+web: gunicorn tweetly_project.wsgi --bind 0.0.0.0:$PORT
--- tweetly_project/urls.py
+++ tweetly_project/urls.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('', lambda request: redirect('/tweets/')),
     path('tweets/', include('tweets.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
+    re_path(r'^images/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG:
